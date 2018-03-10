4305d8a4222edd99fcdb51471251743e0c46a344
bernd.vanvelsen <bernd.vanvelsen@aca-it.be>
2018-03-10 12:47:17 +0100

    temp shelve

---
+++ backend/src/main/java/be/dispatcher/managers/incidentscene/IncidentSceneManager.java
@@ -0,0 +1,48 @@
+package be.dispatcher.managers.incidentscene;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import be.dispatcher.domain.incident.Incident;
+import be.dispatcher.domain.people.Victim;
+import be.dispatcher.domain.vehicle.MedicalVehicle;
+import be.dispatcher.domain.vehicle.Vehicle;
+
+@Component
+public class IncidentSceneManager {
+
+	private Map<Vehicle, Victim> vehicleVictimCoupling = new HashMap<>();
+
+	public Victim getVictimFor(Vehicle vehicle) {
+		Victim victim = vehicleVictimCoupling.get(vehicle);
+		if (victim == null) {
+			victim = getUntreatedVictimInWorstCondition(vehicle.getIncident());
+			if (victim != null) {
+				vehicleVictimCoupling.put(vehicle, victim);
+			}
+		}
+		return victim;
+	}
+
+	public void fillEmptySlotsWithStableVictims(MedicalVehicle medicalVehicle) {
+		new AmbulanceFreeSpaceFiller(medicalVehicle).fillEmptySlotsWithStableVictims();
+	}
+
+	public void notifyVictimReadyForTransport(Vehicle vehicle) {
+		Incident incident = vehicle.getIncident();
+		Victim victim = vehicleVictimCoupling.remove(vehicle);
+		incident.notifyVictimStabilized(victim);
+	}
+
+	private Victim getUntreatedVictimInWorstCondition(Incident incident) {
+		Optional<Victim> victimOptional = incident.getUnstabilizedVictims().stream().min(Comparator.comparing(victim -> victim.getHealth()));
+		if (victimOptional.isPresent()) {
+			return victimOptional.get();
+		}
+		return null;
+	}
+}
